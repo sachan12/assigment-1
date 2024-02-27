34d803d812ae22ac8c1c921e9908d7ec8d6d9f5d
sachan12 <sachinnanda427@gmail.com>
2024-02-26 19:19:10 -0800

    add files

---
+++ app.js
@@ -0,0 +1,78 @@
+// Chapter -1
+// Question-1
+console.log("Error!Please enter a valid password.") ;
+// Question - 2 
+console.log ("Welcome to JS Land... \n Happy Coding!");
+// Question -3 
+console.log ("Welcome to JS Land...");
+console.log ("Happy Coding! \n Prevent this page from creating additional dialogs");
+// Question-4
+console.log("Hello...I can run JS through my web browser's console");
+// Chapter-2
+// Question-1
+var username="Sachan";
+console.log (username)
+// Question - 2 
+var myname="Sachan khemchand"
+console.log (myname)
+// Question - 3
+var message="Hello World"
+console.log(message) ;
+// Question - 4
+var myname="Sachan khemchand"
+console.log (myname)
+var age="18 years old"
+console.log (age)
+var course="web and mob-application Development"
+console.log (course)
+// Question - 5
+var food="Pizza\n Pizz\n Piz\n Pi\n P\n"
+console.log (food)
+// Question - 6
+var email="my email address is sachinnanda427@gmail.com"
+console.log (email)
+// Question - 7
+var book="A Smarter way to learn javascript"
+console.log (book)
+// Question - 8
+document.write("Yah! I Can write HTML Content through Javascript <br/>")
+// Question - 9
+console.log("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”") ;
+// Chapter-3
+// Question-1
+var age=" I am 18 years old"
+console.log (age)
+// Question - 2 
+var visitor="You have visited this site 14 times"
+console.log (visitor)
+// Question - 3
+document.write("My Birth Year is 2005 <br /> Date type of my ddeclared variable is number <br />")
+// Question - 4
+document.write("Sachan ordered 5-TShirt(s) on xyx clothing store")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
